c9d3ef9175c18e0045f1e736b28326968dab0d9e
MTalhaE53 <100591024+MTalhaE53@users.noreply.github.com>
2025-06-20 14:38:09 +0300

    Add files via upload

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,15 @@
-const { Pool } = require("pg");
-require("dotenv").config();
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { Pool } = pg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Render gerektirir
+  },
 });
 
-module.exports = pool;
+export default pool;
